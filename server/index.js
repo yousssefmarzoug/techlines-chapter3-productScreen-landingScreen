9382ddcc2e727df9f7402d71cb34e6a647dfdbf8
youssef marzoug <youssefmarzoug19@gmail.com>
2024-02-07 23:31:24 +0100

    change index

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 import connectToDatabase from './db.js';
 import express from 'express';
 import cors from 'cors';
-
+import path from 'path';
 // Routes
 import productRoutes from './routes/productRoutes.js';
 
@@ -15,6 +15,14 @@ app.use(cors());
 app.use('/api/products', productRoutes);
 
 const port = 5000;
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
 
 app.get('/', (req, res) => {
 	res.send('Api is running...');
